76babc207875a3cbfe229193287bd146b0e32a11
imamknitto <it14.knitto@gmail.com>
2026-04-10 09:19:25 +0700

    update config

--- be-server/lib/config.js
+++ be-server/lib/config.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 
 const PORT = Number(process.env.PORT) || 8080;
 const SYNOLOGY_BASE_URL = (
-  process.env.SYNOLOGY_BASE_URL || "http://192.168.20.25:8101"
+  process.env.SYNOLOGY_BASE_URL || "http://"
 ).replace(/\/$/, "");
 const FOLDER_PATH =
-  process.env.SYNOLOGY_FOLDER_PATH || "imam/electron-update-server";
-const BASE_VOLUME = process.env.SYNOLOGY_BASE_VOLUME || "Public_Holis";
+  process.env.SYNOLOGY_FOLDER_PATH || "imam/...";
+const BASE_VOLUME = process.env.SYNOLOGY_BASE_VOLUME || "...";
 const LATEST_YML_OVERRIDE = process.env.SYNOLOGY_LATEST_YML_URL || "";
 const API_TOKEN = process.env.SYNOLOGY_API_TOKEN || "";
 
